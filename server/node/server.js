16d1f14189eae7907a29392c74882ed58a247f1c
changwoo1223 <changwoo041223@naver.com>
2025-04-14 15:57:22 +0900

    feat: node.js server 기능추가

--- server/node/server.js
+++ server/node/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = 3000; 
 const cors = require('cors');
 app.use(cors());
 
+const distPath = path.join(__dirname, '../../client/dist');
 
+// 정적 파일 경로 설정 (React 빌드된 결과 제공)
+app.use(express.static(distPath));
 
-
+// 루트 경로로 접속 시 index.html 보내기
+app.get('*', (req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+    });
 
 // 서버 실행
 app.listen(PORT, () => {
