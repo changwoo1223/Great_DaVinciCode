4e3bb2ec798e427c5ada693082c75554c51a264d
dakori9480 <kgh9480@naver.com>
2025-04-15 11:49:33 +0900

    추측, 턴 함수 추가

--- server/python/main.py
+++ server/python/main.py
@@ -16,6 +16,7 @@ hide_index_list = [[] for _ in range(player_count)]
 
 # 타일 정렬 기준
 def sort_tile(tile):
+    # 추후 수정정
     if tile == 'jb':
         return (0, 0)
     if tile == 'jw':
@@ -56,7 +57,7 @@ def display_tiles(hand, hide_index):
     return display
 
 # 타일 추측 함수
-def guess_tile(players, guess_player_index):
+def guess_tile(players, guess_player_index, draw_tile=None):
     target_index = int(input('어느 플레이어를 선택하시겠습니까? > '))
     print(f'{guess_player_index}번 플레이어가 {target_index}번 플레이어를 선택하였습니다.')
 
@@ -71,9 +72,40 @@ def guess_tile(players, guess_player_index):
         hide_index_list[target_index].append(target_tile_index)
     else:
         print('틀렸습니다!')
+        if draw_tile:
+            # 드로우한 타일이 플레이어 손패의 몇 번째에 들어갔는지 찾음
+            tile_index = players[guess_player_index].index(draw_tile)
+            hide_index_list[guess_player_index].append(tile_index)
+            
+def player_turn(players, player_index):
+    print(f"\n=== {player_index}번 플레이어의 턴입니다 ===")
+    drawn_tile = draw_turn(players[player_index])
 
+    # 현재 전체 손패 상황 출력
+    for i, hand in enumerate(players):
+        print(f'{i}번 손패: {display_tiles(hand, hide_index_list[i])}')
 
+    guessed_correctly = guess_tile(players, player_index, drawn_tile)
+
+    if guessed_correctly:
+        while True:
+            choice = input("1. 한번 더 추리하기\n2. 턴 넘기기\n선택하세요 (1 또는 2): ")
+            if choice == "1":
+                print(f"{player_index}번 플레이어가 다시 추리를 시도합니다!")
+                return player_index  # 턴 유지
+            elif choice == "2":
+                print("턴을 넘깁니다.")
+                return (player_index + 1) % player_count  # 다음 플레이어로
+            else:
+                print("잘못된 입력입니다. 다시 선택해주세요.")
+    else:
+        print("틀렸습니다. 턴이 다음 플레이어에게 넘어갑니다.")
+        return (player_index + 1) % player_count  # 틀리면 자동 턴 넘김
 
 start_tile(players)
-draw_turn = draw_turn(players[0])
-guess_tile(players, 0)
+
+while True:
+    current_player = player_turn(players, current_player)
+    
+# for i, hand in enumerate(players):
+#     print(f'{i}, {hand}')
